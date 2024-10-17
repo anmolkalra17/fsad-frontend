690c5eb12074bdf37af7b27dd3f8fe6841507cf0
anmolkalra <anmolkalra17@gmail.com>
2024-10-17 17:57:24 +0530

    Add search bar in book list

--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../auth/AuthContext';
+import { AuthContext } from '../auth/AuthContext'; // Adjust the import path as needed
 import './BookList.css';
 
 const BookList = () => {
   const { logout } = useContext(AuthContext);
   const [books, setBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
 	fetch('http://localhost:8801/api/books/')
@@ -13,12 +14,23 @@ const BookList = () => {
 	  .catch(error => console.error('Error fetching books:', error));
   }, []);
 
+  const filteredBooks = books.filter(book =>
+	book.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
 	<div className="booklist-container">
 	  <h1 className="booklist-title">Books List</h1>
 	  <button className="logout-button" onClick={logout}>Logout</button>
+	  <input
+		type="text"
+		className="search-bar"
+		placeholder="Search books..."
+		value={searchQuery}
+		onChange={(e) => setSearchQuery(e.target.value)}
+	  />
 	  <ul className="booklist">
-		{books.map(book => (
+		{filteredBooks.map(book => (
 		  <li key={book.id} className="booklist-item">{book.title}</li>
 		))}
 	  </ul>
