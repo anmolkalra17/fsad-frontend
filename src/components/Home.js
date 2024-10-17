cb752216af502dfeeb388fb3b4eaa7e56c72b69b
anmolkalra <anmolkalra17@gmail.com>
2024-10-17 19:11:17 +0530

    Update UI in booklist and change path for booklist

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
 		.then(data => setBooks(data))
 		.catch(error => console.error('Error fetching books:', error));
 	  
-	  navigate('/booklist');
+	  navigate('/books');
 	}
   }, [isAuthenticated, navigate]);
 
--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -4,51 +4,51 @@ import { AuthContext } from '../auth/AuthContext';
 import './BookList.css';
 
 const BookList = () => {
-  const { logout } = useContext(AuthContext);
-  const [books, setBooks] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const navigate = useNavigate();
+	const { logout } = useContext(AuthContext);
+	const [books, setBooks] = useState([]);
+	const [searchQuery, setSearchQuery] = useState('');
+	const navigate = useNavigate();
 
-  useEffect(() => {
-	fetch('http://localhost:8801/api/books/')
-	  .then(response => response.json())
-	  .then(data => setBooks(data))
-	  .catch(error => console.error('Error fetching books:', error));
-  }, []);
+	useEffect(() => {
+		fetch('http://localhost:8801/api/books/')
+			.then(response => response.json())
+			.then(data => setBooks(data))
+			.catch(error => console.error('Error fetching books:', error));
+	}, []);
 
-  const filteredBooks = books.filter(book =>
-	book.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+	const filteredBooks = books.filter(book =>
+		book.title.toLowerCase().includes(searchQuery.toLowerCase())
+	);
 
-  const handleBookClick = (uuid) => {
-	console.log("Clicked on book id: ", uuid);
-	navigate(`/book/${uuid}`);
-  };
+	const handleBookClick = (uuid) => {
+		console.log("Clicked on book id: ", uuid);
+		navigate(`/book/${uuid}`);
+	};
 
-  return (
-	<div className="booklist-container">
-	  <h1 className="booklist-title">Books List</h1>
-	  <button className="logout-button" onClick={logout}>Logout</button>
-	  <input
-		type="text"
-		className="search-bar"
-		placeholder="Search books..."
-		value={searchQuery}
-		onChange={(e) => setSearchQuery(e.target.value)}
-	  />
-	  <ul className="booklist">
-		{(filteredBooks.length > 0 ? filteredBooks : books).map(book => ( 
-		  <li
-			key={book.uuid}
-			className="booklist-item"
-			onClick={() => handleBookClick(book.uuid)}
-		  >
-			{book.title}
-		  </li>
-		))}
-	  </ul>
-	</div>
-  );
+	return (
+		<div className="booklist-container">
+			<button className="logout-button" onClick={logout}>Logout</button>
+			<h1 className="booklist-title">Books List</h1>
+			<input
+				type="text"
+				className="search-bar"
+				placeholder="Search books..."
+				value={searchQuery}
+				onChange={(e) => setSearchQuery(e.target.value)}
+			/>
+			<ul className="booklist">
+				{(filteredBooks.length > 0 ? filteredBooks : books).map(book => (
+					<li
+						key={book.uuid}
+						className="booklist-item"
+						onClick={() => handleBookClick(book.uuid)}
+					>
+						{book.title}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default BookList;
